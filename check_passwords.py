a6963e0aba3b7f3a1eb8e8247a7d88cfa3ce0376
Kfir <kfir.carmely2@gmail.com>
2025-07-23 23:36:30 +0300

    most of the code

---
+++ check_passwords.py
@@ -0,0 +1,44 @@
+# Globals
+
+COMMON_PASSWORDS = [
+    "123456",
+    "password",
+    "123456789",
+    "12345678",
+    "12345",
+    "123123",
+    "qwerty",
+    "abc123",
+    "password1",
+    "111111"
+]
+
+
+def check_password(password): # Main Checking function
+    counter = 0
+    counter += check_length(len(password))
+
+    counter += 1 if password.islower() else 0 # Checks if contains lower case
+    counter += 1 if password.isupper() else 0 # Checks if contains upper case
+    counter += 1 if any(i.isdigit() for i in password) else 0 # Checks if contains numbers
+    counter += 1 if any(not c.isalnum() for c in password) else 0 # Checks if contains special characters
+
+    counter += check_common(password) # Checks if password is common
+
+    return counter
+
+def check_common(password): # Checks if password is common
+    return -2 if password in COMMON_PASSWORDS else 0
+
+def check_length(l): # Returns points on the basis of the Length of the password
+    if l < 8:
+        return 0
+
+    elif 8 < l < 11:
+        return 1
+
+    return 2
+
+if __name__ == '__main__':
+    pas = input('Enter password: ')
+    check_password(pas)
